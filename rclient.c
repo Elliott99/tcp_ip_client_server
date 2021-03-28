db46fd05117956c8a8cc9f5f59e89fd18805dd15
Elliott <eg9699@bard.edu>
2021-03-28 19:39:42 -0400

    The working server file

--- rclient.c
+++ rclient.c
@@ -5,6 +5,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include "struct.h"
+
+#define MAX_MESSAGE_LENGTH 200
+void chat_function(int sockfd,char *write_buffer,char *read_buffer){
+	printf("\nEnter a message to send to the server. The server will reverse it, and send it back\n");
+	fscanf(stdin,"%s",write_buffer);
+	int m=strlen(write_buffer);
+	write_buffer[m]='\0';
+	int i=0;
+	ssize_t message_size;
+	ssize_t received_size;
+	//write out the length of the string read in plus 1 for the null terminator at the end
+	message_size=send(sockfd,write_buffer,strlen(write_buffer)+1,0);
+	recv(sockfd,read_buffer,strlen(write_buffer)+1,0);
+	int j=strlen(read_buffer);
+	read_buffer[j]='\0';
+	int z=0;
+	printf("Reversed server response: %s\n",read_buffer);
+}
+
+
 
 int main(int argc, char*argv[]){
 	struct addrinfo hints;
@@ -18,35 +39,30 @@ int main(int argc, char*argv[]){
 	hints.ai_canonname=NULL;
 	hints.ai_addr=NULL;
 	hints.ai_flags=AI_NUMERICSERV;
-	//Command line argument must be a hostname for the client
+	char addbuff[NI_MAXSERV+NI_MAXHOST+10];
+	char write_buffer[MAX_MESSAGE_LENGTH];
+	char read_buffer[MAX_MESSAGE_LENGTH];
+	bzero(write_buffer,sizeof(write_buffer));
+	bzero(read_buffer,sizeof(read_buffer));
 	if (argc<2){
 		printf("Failed to give hostname for client");
 		exit(EXIT_FAILURE);
 	} 
-	//call getaddrinfo to attempt to search for addresses that our socket can eventually connect to
-	//should use the "localhost" host name here as a command line address
 	if ((getaddrinfo(argv[1],PORT_NUM,&hints,&result))!=0){
 		printf("You did not provide a legitimate host name for the client socket to search through addresses to connect to");
 		exit(EXIT_FAILURE);
 	}
-	//loop through the possible addresses generated from getaddrinfo until we successfully can connect to one
-	//bind() is not needed for clients: we are interested only in connceting to a different endpoint, and the kernel will handle,
-	//through the routing table, establishing an IP address and port number for our client
  	for (rp=result;rp;rp=rp->ai_next){
 		cfd=socket(rp->ai_family,rp->ai_socktype, rp->ai_protocol);
 		if (cfd==-1){
 			continue;
 		}
 		if (connect(cfd,rp->ai_addr,rp->ai_addrlen)!=-1){
-			int error = 0;
-			socklen_t len = sizeof (error);
-			int retval = getsockopt (cfd, SOL_SOCKET, SO_ERROR, &error, &len);
-			if (retval==0){
-				printf("Socket successfully connected to client from server\n");
-			}
 			break;
 		}
 	}
+	chat_function(cfd,write_buffer,read_buffer);
+
 	if (rp==NULL){
 		printf("Could not connect socket to any address");
 		exit(EXIT_FAILURE);
